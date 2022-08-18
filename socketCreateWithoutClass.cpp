c38e044855c08716323ff7c01a45ad0c0dff28ec
Ankush raj <104245302+AnkushTech2022@users.noreply.github.com>
2022-08-18 17:01:47 +0530

    Update socketCreateWithoutClass.cpp

--- socketCreateWithoutClass.cpp
+++ socketCreateWithoutClass.cpp
@@ -1,1 +1,19 @@
 
+#include<iostream>
+#include<sys/socket.h>
+#include<sys/type.h>
+#include<unistd.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+using namespace std;
+int main(){
+  int sSocket=socket(AF_INET, SOCK_STREAM, 0);
+  if(sSocket<0){
+    cout<<"socket wasn't created"<<endl;
+    return -1;
+  }
+  else{
+    cout<<"socket created successfully"<<endl;
+  }
+  return 0;
+}
