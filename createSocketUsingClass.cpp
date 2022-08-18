8678fc6af17d97ecce701f111a6f97ed6d16b030
Ankush raj <104245302+AnkushTech2022@users.noreply.github.com>
2022-08-18 17:09:28 +0530

    Update createSocketUsingClass.cpp

--- createSocketUsingClass.cpp
+++ createSocketUsingClass.cpp
@@ -1,1 +1,30 @@
 
+#include<iostream>
+#include<sys/type.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<netdb.h>
+#include<arpa/inet.h>
+using namespace std;
+
+class createSocket{
+  public:
+  int sSocket;
+  int serverSocket(){
+  if(sSocket<0){
+  cout<<"socket can't be created"<<endl;
+    return -1;
+  }
+    else{
+    cout<<"socket created succesfully"<<endl;
+    }
+    return sSocket;
+  }
+};
+int main(){
+createSocket cSock;
+  cSock.sSocket=socket(AF_INET, SOCK_STREAM, 0);
+  cSock.serverSocket();
+  return 0;
+}
+o/p: socket created successfully.
