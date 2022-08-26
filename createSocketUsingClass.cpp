1a603a0ccb2c4088b69dc9e8a09ff428d27976ce
Ankush raj <104245302+AnkushTech2022@users.noreply.github.com>
2022-08-26 09:25:14 +0530

    Update createSocketUsingClass.cpp

--- createSocketUsingClass.cpp
+++ createSocketUsingClass.cpp
@@ -1,30 +1,53 @@
-
+#ifdef linux
+#define platform_name "Linux"
 #include<iostream>
-#include<sys/type.h>
+#include<bits/stdc++.h>
 #include<sys/socket.h>
+#include<sys/types.h>
 #include<unistd.h>
+#include<error>
 #include<netdb.h>
 #include<arpa/inet.h>
+#include "../linuxPlatform.h"
 using namespace std;
+#endif
 
-class createSocket{
-  public:
-  int sSocket;
-  int serverSocket(){
-  if(sSocket<0){
-  cout<<"socket can't be created"<<endl;
-    return -1;
-  }
-    else{
-    cout<<"socket created succesfully"<<endl;
-    }
-    return sSocket;
-  }
+
+#ifdef linux
+class linuxPlatform {
+public:
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+	int createSocket() {
+		if (sockfd < 0) {
+			std::cerr << "socket creation failed......" << strerror(errno) << std::endl;
+		}
+		else {
+			std::cout << "socket created successfully....." << std::endl;
+		}
+		return sockfd;
+	}
 };
-int main(){
-createSocket cSock;
-  cSock.sSocket=socket(AF_INET, SOCK_STREAM, 0);
-  cSock.serverSocket();
-  return 0;
-}
-o/p: socket created successfully.
+
+
+class bindPoint: public linuxPlatform{
+	struct sockaddr_in serverAddr;
+	int binding;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(port);
+	serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	int bindStatus = bind(serverSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+	int bindSocketStatus() {
+		if (bindstatud < 0) {
+			std::cerr << "socket binding has failed" << strerror(errno);
+			return 0;
+		}
+	}
+
+
+};
+
+#endif // linux
+
+
+
